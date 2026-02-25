c966237c14770957d87a66cf1ecaf792672f0f3f
HasinAbrer <hasinjunior12@gmail.com>
2026-02-25 16:43:45 +0600

    improve the structure of the blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,39 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-    console.log(blog)
-    return (
-        <div>
-            
+const Blog = ({ blog }) => {
+  // console.log(blog)
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    reading_time,
+    posted_date,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} alt={`Cover picture of the title ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14 rounded-3xl" src={author_img} alt="" />
+          <div className="ml-[20px]">
+            <h3 className="text-xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>{hashtags.join(" ")}</p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
