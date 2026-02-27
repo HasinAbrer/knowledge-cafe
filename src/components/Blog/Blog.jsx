59208439345d7973a435f4af26457ff1de860a50
HasinAbrer <hasinjunior12@gmail.com>
2026-02-27 15:03:21 +0600

    add bookmark and declear state for the bookmark button

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
+import { IoBookmarkOutline } from "react-icons/io5";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
   // console.log(blog)
   const {
     title,
@@ -12,9 +13,9 @@ const Blog = ({ blog }) => {
     hashtags,
   } = blog;
   return (
-    <div>
-      <img src={cover} alt={`Cover picture of the title ${title}`} />
-      <div className="flex justify-between">
+    <div className="mb-20">
+      <img className="w-full mb-8" src={cover} alt={`Cover picture of the title ${title}`} />
+      <div className="flex justify-between mb-4">
         <div className="flex">
           <img className="w-14 rounded-3xl" src={author_img} alt="" />
           <div className="ml-[20px]">
@@ -22,8 +23,9 @@ const Blog = ({ blog }) => {
             <p>{posted_date}</p>
           </div>
         </div>
-        <div>
+        <div className="flex items-center justify-center">
           <span>{reading_time} min read</span>
+          <button onClick={handleAddToBookmark} className="ml-2"><IoBookmarkOutline /></button>
         </div>
       </div>
       <h2 className="text-4xl">{title}</h2>
